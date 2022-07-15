d138c7b4f4a542edbfd11214739981c87cc18c18
MarioDev <mariodev@localhost.localdomain>
2022-07-15 18:35:09 -0400

    commitando a parte do teste

--- test/api_rest_app_test.dart
+++ test/api_rest_app_test.dart
@@ -2,7 +2,7 @@ import 'package:api_rest_app/api_rest_app.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
+  // test('calculate', () {
+  //   expect(calculate(), 42);
+  // });
 }
